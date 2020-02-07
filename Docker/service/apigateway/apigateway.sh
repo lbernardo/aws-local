a18f90cfad80f2de9892516cf1c88a63336c2204
Lucas Bernardo <lucas080795@hotmail.com>
2020-02-07 13:24:09 -0300

    Docker api gateway

--- Docker/service/apigateway/apigateway.sh
+++ Docker/service/apigateway/apigateway.sh
@@ -1,2 +1,30 @@
 #!/bin/sh
-awslocal api-gateway --port $PORT --host $HOST --volume $VOLUME
+make build
+
+P_PORT=""
+P_HOST=""
+P_NET=""
+P_ENV=""
+P_SERVERLESS=""
+
+if [ $PORT != "" ] ; then
+  P_PORT="--port $PORT"
+fi
+
+if [ $HOST != "" ] ; then
+  P_HOST="--host $HOST"
+fi
+
+if [ $NETWORK != "" ] ; then
+  P_NET="--network $NETWORK"
+fi
+
+if [ $ENVFILE != "" ] ; then
+  P_ENV="--env $ENVFILE"
+fi
+
+if [ $SERVERLESS != "" ] ; then
+  P_SERVERLESS="--yaml $SERVERLESS"
+fi
+
+awslocal api-gateway $P_PORT $P_HOST --volume $VOLUME $P_NET $P_ENV $P_SERVERLESS
